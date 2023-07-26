dd2b1f7b6816abf8fad5e81568f5f398547cfc77
Rishika1412 <gargrishika5@gmail.com>
2023-07-26 11:20:33 +0530

    v1.0.2+3

--- android/app/src/main/kotlin/com/hmz/folio/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.rishi.portfolio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/about_utils.dart
+++
@@ -1,7 +0,0 @@
-class AboutUtils {
-  static const String aboutMeHeadline =
-      'I\'m Rishika Garg, a Flutter developer, a creative problem-solver, and a dedicated technophile.';
-
-  static const String aboutMeDetail =
-      "A B.Tech CSE major at AKGEC, I am a dedicated Flutter developer with experience in creating diverse mobile apps. My work, spanning from a user-friendly pregnancy app to a finance management app, showcases my technical skills and user-focused approach. Eager to continually learn and stay updated with latest tech trends, I actively seek challenging opportunities to make a meaningful contribution in mobile app development. With a unique blend of technical expertise, innovative thinking, and customer engagement, I aim to deliver significant value in any project I undertake. Let's connect and explore how my skills can contribute to your success.";
-}
